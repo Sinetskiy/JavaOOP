b700c23be15a10b9a53a111bc73fc598fdae6f6a
Andrey.Sinetskiy <asinetsky@tsft.ru>
2017-11-25 23:44:17 +0300

    constructors

--- src/com/sinetskiy/fastjava2/main/Start.java
+++ src/com/sinetskiy/fastjava2/main/Start.java
@@ -1,9 +1,18 @@
 package com.sinetskiy.fastjava2.main;
 
+import com.sinetskiy.fastjava2.objects.Phone;
+
 public class Start {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi!");
+        Phone phone = new Phone(1024, "Samsung", "GSM");
+
+        System.out.println("phone.getName() = " + phone.getName());
+
+        phone.setName("iPhone");
+
+        System.out.println("phone.getName() = " + phone.getName());
+        System.out.println("phone.getType() = " + phone.getType());
     }
 }
